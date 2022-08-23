c9039e4b9ee30dd2dd8ad22a4c37032d951cead0
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2022-08-23 21:47:19 +0900

    Refactor tests

--- src/eval.test.ts
+++ src/eval.test.ts
@@ -8,264 +8,175 @@ import { TranspileError } from "./types";
 import { base } from "./lib/base";
 
 describe("evalForm", () => {
-  test("`( plusF 2.0 (timesF 3.0 4.0) )`", () => {
-    expect(
-      evalForm(
-        assertNonError(readStr("( plusF 2.0 (timesF 3.0 4.0) )")),
-        Env.init(base())
-      )
-    ).toEqual(14);
-  });
+  function testOf({ src, expected }: { src: string; expected: any }): void {
+    test(`\`${src}\` => ${expected}`, () => {
+      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
+        expected
+      );
+    });
+  }
 
-  test('`(eval "1")`', () => {
-    const src = '(eval "1")';
-    expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-      new TranspileError('No function "eval" is defined!')
-    );
+  testOf({ src: "( plusF 2.0 (timesF 3.0 4.0) )", expected: 14 });
+  testOf({
+    src: '(eval "1")',
+    expected: new TranspileError('No function "eval" is defined!'),
   });
-
-  test("`(plusF eval eval)`", () => {
-    const src = "(plusF eval eval)";
-    expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-      new TranspileError('No variable "eval" is defined!')
-    );
+  testOf({
+    src: "(plusF eval eval)",
+    expected: new TranspileError('No variable "eval" is defined!'),
   });
 
   describe("(if bool x y)", () => {
-    test("`(if True 1 2)`", () => {
-      const src = "(if True 1 2)";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        1
-      );
-    });
-    test("`(if False 1 2)`", () => {
-      const src = "(if False 1 2)";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        2
-      );
-    });
+    testOf({ src: "(if True 1 2)", expected: 1 });
+    testOf({ src: "(if False 1 2)", expected: 2 });
   });
 
   describe("(fn (a r g s) (f) (o) (r) (m) (s))", () => {
-    test("`( (fn (x) (plusF x 3)) 2 )` => 5", () => {
-      const src = "( (fn (x) (plusF x 3)) 2 )";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        5
-      );
-    });
+    testOf({ src: "( (fn (x) (plusF x 3)) 2 )", expected: 5 });
   });
 
   describe("(equals x y)", () => {
-    test('`(scope (const x "123") (equals x "123"))`', () => {
-      const src = '(scope (const x "123") (equals x "123"))';
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        true
-      );
-    });
+    testOf({ src: '(scope (const x "123") (equals x "123"))', expected: true });
 
-    test('`(scope (const x "123") (equals x 123))`', () => {
-      const src = '(scope (const x "123") (equals x 123))';
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        false
-      );
-    });
+    testOf({ src: '(scope (const x "123") (equals x 123))', expected: false });
   });
 
   describe("(notEquals x y)", () => {
-    test("`(scope (const x 123) (notEquals x 123))`", () => {
-      const src = "(scope (const x 123) (notEquals x 123))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        false
-      );
-    });
-
-    test('`(scope (const x 123) (notEquals x "123"))`', () => {
-      const src = '(scope (const x 123) (notEquals x "123"))';
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        true
-      );
+    testOf({ src: "(scope (const x 123) (notEquals x 123))", expected: false });
+    testOf({
+      src: '(scope (const x 123) (notEquals x "123"))',
+      expected: true,
     });
   });
 
   describe("(isLessThan x y)", () => {
-    test("`(scope (const x 123) (isLessThan x 124))`", () => {
-      const src = "(scope (const x 123) (isLessThan x 124))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        true
-      );
+    testOf({ src: "(scope (const x 123) (isLessThan x 124))", expected: true });
+
+    testOf({
+      src: "(scope (const x 123) (isLessThan x 123))",
+      expected: false,
     });
 
-    test("`(scope (const x 123) (isLessThan x 123))`", () => {
-      const src = "(scope (const x 123) (isLessThan x 123))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        false
-      );
-    });
-
-    test("`(scope (const x 123) (isLessThan x 122))`", () => {
-      const src = "(scope (const x 123) (isLessThan x 122))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        false
-      );
+    testOf({
+      src: "(scope (const x 123) (isLessThan x 122))",
+      expected: false,
     });
   });
 
   describe("(isLessThanOrEquals x y)", () => {
-    test("`(scope (const x 123) (isLessThanOrEquals x 124))`", () => {
-      const src = "(scope (const x 123) (isLessThanOrEquals x 124))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        true
-      );
+    testOf({
+      src: "(scope (const x 123) (isLessThanOrEquals x 124))",
+      expected: true,
     });
 
-    test("`(scope (const x 123) (isLessThanOrEquals x 123))`", () => {
-      const src = "(scope (const x 123) (isLessThanOrEquals x 123))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        true
-      );
+    testOf({
+      src: "(scope (const x 123) (isLessThanOrEquals x 123))",
+      expected: true,
     });
 
-    test("`(scope (const x 123) (isLessThanOrEquals x 122))`", () => {
-      const src = "(scope (const x 123) (isLessThanOrEquals x 122))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        false
-      );
+    testOf({
+      src: "(scope (const x 123) (isLessThanOrEquals x 122))",
+      expected: false,
     });
   });
 
   describe("(isGreaterThan x y)", () => {
-    test("`(scope (const x 123) (isGreaterThan x 124))`", () => {
-      const src = "(scope (const x 123) (isGreaterThan x 124))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        false
-      );
+    testOf({
+      src: "(scope (const x 123) (isGreaterThan x 124))",
+      expected: false,
     });
 
-    test("`(scope (const x 123) (isGreaterThan x 123))`", () => {
-      const src = "(scope (const x 123) (isGreaterThan x 123))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        false
-      );
+    testOf({
+      src: "(scope (const x 123) (isGreaterThan x 123))",
+      expected: false,
     });
 
-    test("`(scope (const x 123) (isGreaterThan x 122))`", () => {
-      const src = "(scope (const x 123) (isGreaterThan x 122))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        true
-      );
+    testOf({
+      src: "(scope (const x 123) (isGreaterThan x 122))",
+      expected: true,
     });
   });
 
   describe("(isGreaterThanOrEquals x y)", () => {
-    test("`(scope (const x 123) (isGreaterThanOrEquals x 124))`", () => {
-      const src = "(scope (const x 123) (isGreaterThanOrEquals x 124))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        false
-      );
+    testOf({
+      src: "(scope (const x 123) (isGreaterThanOrEquals x 124))",
+      expected: false,
     });
 
-    test("`(scope (const x 123) (isGreaterThanOrEquals x 123))`", () => {
-      const src = "(scope (const x 123) (isGreaterThanOrEquals x 123))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        true
-      );
+    testOf({
+      src: "(scope (const x 123) (isGreaterThanOrEquals x 123))",
+      expected: true,
     });
 
-    test("`(scope (const x 123) (isGreaterThanOrEquals x 122))`", () => {
-      const src = "(scope (const x 123) (isGreaterThanOrEquals x 122))";
-      expect(evalForm(assertNonError(readStr(src)), Env.init(base()))).toEqual(
-        true
-      );
+    testOf({
+      src: "(scope (const x 123) (isGreaterThanOrEquals x 122))",
+      expected: true,
     });
   });
 });
 
 describe("evalBlock", () => {
-  test("`(const x (timesF 3 3))(plusF x 2)`", () => {
-    const src = "(const x (timesF 3 3))(plusF x 2)";
-    expect(
-      assertNonError(
+  function testOf({ src, expected }: { src: string; expected: any }): void {
+    test(`\`${src}\` => ${expected}`, () => {
+      expect(
         evalBlock(assertNonError(readBlock(src)), Env.init(base()))
-      )
-    ).toEqual(11);
+      ).toEqual(expected);
+    });
+  }
+
+  testOf({
+    src: "(const x (timesF 3 3))(plusF x 2)",
+    expected: 11,
   });
 
-  test("`(let y (dividedByF 3 2))(assign y (plusF y 2))(minusF y 7)`", () => {
-    const src = "(let y (dividedByF 3 2))(assign y (plusF y 2))(minusF y 6)";
-    expect(
-      assertNonError(
-        evalBlock(assertNonError(readBlock(src)), Env.init(base()))
-      )
-    ).toEqual(-2.5);
+  testOf({
+    src: "(let y (dividedByF 3 2))(assign y (plusF y 2))(minusF y 6)",
+    expected: -2.5,
   });
 
-  test("`(const y 5)(const y 3)`", () => {
-    const src = "(const y 5)(const y 3)";
-    expect(evalBlock(assertNonError(readBlock(src)), Env.init(base()))).toEqual(
-      new TranspileError('Variable "y" is already defined!')
-    );
+  testOf({
+    src: "(const y 5)(const y 3)",
+    expected: new TranspileError('Variable "y" is already defined!'),
   });
 
-  test("`(let y 6)(let y 7)`", () => {
-    const src = "(let y 6)(let y 7)";
-    expect(evalBlock(assertNonError(readBlock(src)), Env.init(base()))).toEqual(
-      new TranspileError('Variable "y" is already defined!')
-    );
+  testOf({
+    src: "(let y 6)(let y 7)",
+    expected: new TranspileError('Variable "y" is already defined!'),
   });
 
-  test("`(const y 5)(scope (const y 3) (timesF y 2))`", () => {
-    const src = "(const y 5)(scope (const y 3) (timesF y 2))";
-    expect(evalBlock(assertNonError(readBlock(src)), Env.init(base()))).toEqual(
-      6
-    );
+  testOf({ src: "(const y 5)(scope (const y 3) (timesF y 2))", expected: 6 });
+
+  testOf({ src: "(let y 6)(scope (let y 7) (dividedByF y 2))", expected: 3.5 });
+
+  testOf({
+    src: "(const y 5)(scope (const y 3) (plusF y 0)) (timesF y 2)",
+    expected: 10,
   });
 
-  test("`(let y 6)(scope (let y 7) (dividedByF y 2))`", () => {
-    const src = "(let y 6)(scope (let y 7) (dividedByF y 2))";
-    expect(evalBlock(assertNonError(readBlock(src)), Env.init(base()))).toEqual(
-      3.5
-    );
+  testOf({
+    src: "(let y 6)(scope (let y 7) (plusF y 0)) (dividedByF y 3)",
+    expected: 2,
   });
 
-  test("`(const y 5)(scope (const y 3) (plusF y 0)) (timesF y 2)`", () => {
-    const src = "(const y 5)(scope (const y 3) (plusF y 0)) (timesF y 2)";
-    expect(evalBlock(assertNonError(readBlock(src)), Env.init(base()))).toEqual(
-      10
-    );
+  testOf({
+    src: "(scope (const y 3))",
+    expected: new TranspileError(
+      "The last statement in a `scope` must be an expression!"
+    ),
   });
 
-  test("`(let y 6)(scope (let y 7) (plusF y 0)) (dividedByF y 3)`", () => {
-    const src = "(let y 6)(scope (let y 7) (plusF y 0)) (dividedByF y 3)";
-    expect(evalBlock(assertNonError(readBlock(src)), Env.init(base()))).toEqual(
-      2
-    );
-  });
-
-  test("`(scope (const y 3))`", () => {
-    const src = "(scope (const y 3))";
-    expect(evalBlock(assertNonError(readBlock(src)), Env.init(base()))).toEqual(
-      new TranspileError(
-        "The last statement in a `scope` must be an expression!"
-      )
-    );
-  });
-
-  test("`(scope (let x 7) (let y 7))`", () => {
-    const src = "(scope (let x 7) (let y 7))";
-    expect(evalBlock(assertNonError(readBlock(src)), Env.init(base()))).toEqual(
-      new TranspileError(
-        "The last statement in a `scope` must be an expression!"
-      )
-    );
+  testOf({
+    src: "(scope (let x 7) (let y 7))",
+    expected: new TranspileError(
+      "The last statement in a `scope` must be an expression!"
+    ),
   });
 
   describe("(fn (a r g s) (f) (o) (r) (m) (s))", () => {
-    test("`(const a 2.5) (const f (fn (x) (const b 3) (minusF (timesF a x) b))) (f 9)` => 19.5", () => {
-      const src =
-        "(const a 2.5) (const f (fn (x) (const b 3) (minusF (timesF a x) b))) (f 9)";
-      expect(
-        evalBlock(assertNonError(readBlock(src)), Env.init(base()))
-      ).toEqual(19.5);
+    testOf({
+      src:
+        "(const a 2.5) (const f (fn (x) (const b 3) (minusF (timesF a x) b))) (f 9)",
+      expected: 19.5,
     });
   });
 });
