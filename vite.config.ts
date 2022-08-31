f319d85da783c8d16a00f65c80101506b1054cc5
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2022-09-01 00:12:47 +0900

    Enable coverage

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@
 
 // Configure Vitest (https://vitest.dev/config/)
 
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
     /* for example, use global to avoid globals imports (describe, test, expect): */
     // globals: true,
+    coverage: {
+      provider: "c8",
+      reporter: ['text', 'json', 'html'],
+    },
   },
-})
+});
