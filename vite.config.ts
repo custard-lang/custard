d16f176b1ae1cd24e2d6b3b62625408662d8c6bd
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2023-04-15 00:32:45 +0000

    Delete @ts-ignore

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     coverage: {
-      // I'd find the problem as soon as I run tests with coverage
-      // if this was really a type error!
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
       provider: "c8",
       reporter: ["text", "json", "html"],
     },
