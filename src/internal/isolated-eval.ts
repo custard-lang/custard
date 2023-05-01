adda0a546ea064d4e87fd5288eadb3f67d2e2951
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2023-05-01 12:50:55 +0900

    Fix: Async Function constructor doesn't seem to work

--- src/internal/isolated-eval.ts
+++ src/internal/isolated-eval.ts
@@ -1,23 +1,17 @@
 // This module is inherently unsafe!
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-return */
 
-import { CU_ENV } from "./cu-env.js";
+import { writeDebugOut } from "../util/debug.js";
 import { Env } from "./types.js";
 
-// Ref. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/AsyncFunction/AsyncFunction
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const _cu$AsyncFunction: {
-  new (argName: string, code: string): (env: Env) => Promise<any>;
-} = async function () {}.constructor as any;
-
+// See cu-env.ts for details of the `_CU$` prefix.
 export const _cu$eval = async (
-  code: string,
-  lastExpression: string,
-  env: Env,
+  _cu$code: string,
+  _cu$lastExpression: string,
+  _cu$env: Env,
 ): Promise<any> => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,  @typescript-eslint/no-unsafe-argument
-  return await new _cu$AsyncFunction(
-    CU_ENV,
-    `${code}return ${lastExpression};`,
-  )(env);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  return await eval(
+    writeDebugOut(`async () => {\n${_cu$code}return ${_cu$lastExpression}\n}`),
+  )();
 };
