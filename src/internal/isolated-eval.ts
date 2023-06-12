8f16af284af23e838c31454b47d34f819e3a0ab6
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2023-06-12 10:06:52 +0900

    Use data URI with `import` to avoid strange behavior of Node.js

--- src/internal/isolated-eval.ts
+++ src/internal/isolated-eval.ts
@@ -1,16 +1,20 @@
 // This module is inherently unsafe!
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-return */
 
+import { CU_ENV } from "./cu-env.js";
 import { Env } from "./types.js";
 
 // See cu-env.ts for details of the `_CU$` prefix.
 export const _cu$eval = async (
-  _cu$code: string,
-  _cu$lastExpression: string,
-  _cu$env: Env,
+  code: string,
+  lastExpression: string,
+  env: Env,
 ): Promise<any> => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  return await eval(
-    `async () => {\n${_cu$code}return ${_cu$lastExpression}\n}`,
-  )();
+  // https://gist.github.com/tomhodgins/0e5a98610a1da2a98049614a4f170734
+  const f = `export default async (${CU_ENV}) => {\n${code}return ${lastExpression}\n}`;
+  const mod = await import(
+    `data:application/javascript,${encodeURIComponent(f)}`
+  );
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,  @typescript-eslint/no-unsafe-member-access
+  return await mod.default(env);
 };
