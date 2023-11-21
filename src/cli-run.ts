cdd4df9ca499505168585c8d68beab6868fad9c1
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2023-11-21 10:45:45 +0900

    Add custard run subcommand

---
+++ src/cli-run.ts
@@ -0,0 +1,30 @@
+import { spawn } from "node:child_process";
+
+import { transpileMain, transpileProgram } from "./cli/common.js";
+
+const result = transpileProgram
+  .option(
+    "-w, --with-engine <path>",
+    "Path to the JavaScript engine to run",
+    "node",
+  )
+  .parse();
+
+(async () => {
+  const [cstdPath, ...args] = result.args;
+  if (cstdPath === undefined) {
+    console.error("No .cstd file specified");
+    process.exit(1);
+  }
+  const opts = result.opts();
+  const [mjsPath] = await transpileMain(opts, [cstdPath]);
+
+  if (mjsPath === undefined) {
+    throw new Error("Assertion failed: no .mjs file transpiled.");
+  }
+
+  if (opts.verbose) {
+    console.log("Running", mjsPath);
+  }
+  spawn(opts.withEngine, ["--", mjsPath, ...args], { stdio: "inherit" });
+})();
--- src/cli.ts
+++ src/cli.ts
@@ -4,6 +4,10 @@ program
   .name("custard")
   .version("0.1.0")
   .command("transpile [files...]", "Transpile .cstd files into .js files.")
+  .command(
+    "run [file] [args...]",
+    "Transpile .cstd files into .js files, then run.",
+  )
   .command("repl [files...]", "Start a Custard REPL session.", {
     isDefault: true,
   })
