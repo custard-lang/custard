489972936f256f6567222d75df4b24d5bc9d3496
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2025-10-23 08:57:42 +0900

    Unroll the loop to simplify the reproduction program

--- packages/command/src/custard-repl.ts
+++ packages/command/src/custard-repl.ts
@@ -4,20 +4,17 @@ import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
 const rl = readline.createInterface({ input, output });
-const n = process.argv[2] ? parseInt(process.argv[2], 10) : 1;
 
 (async () => {
   try {
-    let i = 0;
-    while (true) {
-      if (i === n) {
-        await new Promise((resolve) => setTimeout(resolve, 0));
-      }
-      console.log(`Receiving ${i}`);
-      const answer = await rl.question(`prompt:${i}:>>> `);
-      console.log(answer);
-      i++;
-    }
+    console.log("Receiving 1");
+    console.log(await rl.question("prompt:1:>>> "));
+
+    console.log("Receiving 2");
+    console.log(await rl.question("prompt:2:>>> "));
+
+    console.log("Receiving 3");
+    console.log(await rl.question("prompt:3:>>> "));
   } finally {
     rl.close();
     input.destroy();
