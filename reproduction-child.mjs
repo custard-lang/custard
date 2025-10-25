e575139d42b47cbb7448928d99f9071c9ccbc6ea
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2025-10-25 18:02:19 +0900

    Remove an invalid comment
    Perhaps this means https://github.com/nodejs/node/issues/42581#issuecomment-1086872649

--- reproduction-child.mjs
+++ reproduction-child.mjs
@@ -7,8 +7,6 @@ const rl = readline.createInterface({ input, output });
 const n = process.argv[2] ? parseInt(process.argv[2], 10) : 1;
 
 (async () => {
-  // Uncomment this to confirm that the any `await` makes readline drop the input.
-  // await new Promise((resolve) => { setTimeout(resolve, 0); });
   try {
     for (let i = 0; i < n; i++) {
       console.log(`Receiving ${i}`);
